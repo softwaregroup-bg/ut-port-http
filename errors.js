a466072d3bb9a654d6d5b82b50a43b67313f266c
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-10-19 14:34:50 +0300

    feat(bus): msg.$$ is now a separate parameter named $meta: x(msg) => x(msg, $meta)
    BREAKING CHANGE: msg.$$ is not available anymore. To change the metadata, modify the properties of the last argument to the methods
    In addition all exec methods are now promises and do not expect a callback as parameter

--- errors.js
+++ errors.js
@@ -1,26 +1,37 @@
 var create = require('errno').custom.createError;
-var customErrors = {};
 
-customErrors.HttpClient = {def: create('HttpClient'), message: 'Common error'};
-customErrors.Parser = {def: create('Parser', customErrors.HttpClient.def), message: 'Parser Error'};
-customErrors.MissingContentType = {def: create('MissingContentType', customErrors.Parser.def), message: 'Server returned no content type'};
-customErrors.XmlParser = {def: create('XmlParser', customErrors.Parser.def), message: 'Xml Parser Error'};
-customErrors.JsonParser = {def: create('JsonParser', customErrors.Parser.def), message: 'Json Parser Error'};
-customErrors.ParserNotFound = {def: create('ParserNotFound', customErrors.Parser.def), message: 'Parser Not Found'};
-customErrors.Config = {def: create('Config', customErrors.HttpClient.def), message: 'Configuration error'};
-customErrors.ConfigPropMustdBeSet = {def: create('ConfigPropMustdBeSet', customErrors.Config.def), message: 'Configuration property should be set'};
+var PortHTTP = create('PortHTTP');
+var Parser = create('Parser', PortHTTP);
+var MissingContentType = create('MissingContentType', Parser);
+var XmlParser = create('XmlParser', Parser);
+var JsonParser = create('JsonParser', Parser);
+var ParserNotFound = create('ParserNotFound', Parser);
+var Config = create('Config', PortHTTP);
+var ConfigPropMustBeSet = create('ConfigPropMustBeSet', Config);
 
 module.exports = {
-    errors: customErrors,
-    create: function(hash, cause) {
-        if (!customErrors[hash]) {
-            return new customErrors.HttpClient.def(customErrors.HttpClient.message, cause);
-        }
-        var err = new customErrors[hash].def(customErrors[hash].message, cause);
-        return err;
+    http: function(cause) {
+        return new PortHTTP('HTTP error', cause);
     },
-    createUT5: function(hash, cause) {
-        var err = this.create(hash, cause);
-        return {'$$':{'mtid':'error', 'errorCode':err.name, 'errorMessage':err.message}};
+    parser: function(cause) {
+        return new Parser('Parser Error', cause);
+    },
+    missingContentType: function(cause) {
+        return new MissingContentType('Server returned no content type', cause);
+    },
+    xmlParser: function(cause) {
+        return new XmlParser('Xml Parser Error', cause);
+    },
+    jsonParser: function(cause) {
+        return new JsonParser('Json Parser Error', cause);
+    },
+    parserNotFound: function(cause) {
+        return new ParserNotFound('Parser Not Found', cause);
+    },
+    config: function(cause) {
+        return new Config('Configuration error', cause);
+    },
+    configPropMustBeSet: function(cause) {
+        return new ConfigPropMustBeSet('Configuration property should be set', cause);
     }
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var errors = require('./errors.js');
 var request = (process.type === 'renderer') ? require('browser-request') : require('request');
 var xml2js = require('xml2js');
 var when = require('when');
-var _ = require('lodash');
+var assign = require('lodash/object/assign');
 
 function HttpPort() {
     Port.call(this);
@@ -29,10 +29,11 @@ HttpPort.prototype.start = function start(callback) {
     Port.prototype.start.apply(this, arguments);
     this.pipeExec(this.exec.bind(this), this.config.concurrency);
 };
-HttpPort.prototype.exec = function exec(msg, callback) {
+HttpPort.prototype.exec = function exec(msg) {
+    var $meta = (arguments.length > 1 && arguments[arguments.length - 1]);
     var url = '';
     var self = this;
-    var headers = _.assign({}, this.config.headers, msg.headers);
+    var headers = assign({}, this.config.headers, msg.headers);
     var parseResponse = true;
     if (this.config.parseResponse === false) {
         parseResponse = false;
@@ -41,79 +42,84 @@ HttpPort.prototype.exec = function exec(msg, callback) {
         parseResponse = false;
     }
 
-    //check for required params
-    if (!(url = msg.url || this.config.url)) {
-        callback(errors.createUT5('ConfigPropMustdBeSet', 'url should be set'));
-    } else {
-        url = url + (msg.uri || this.config.uri || '');
-    }
-
-    var connProps = {
-        'followRedirect': false,
-        'method': msg.httpMethod || this.config.method,
-        'url': url,
-        'timeout': msg.requestTimeout || this.config.requestTimeout || 30000,
-        'headers': headers,
-        'body': msg.payload
-    };
-    //if there is a raw config propery it will be merged with `connProps`
-    if (this.config.raw) {
-        _.assign(connProps, this.config.raw);
-    }
-    //do the connection + request
-    request(connProps, function cbresp(error, response, body) {
-        if (error) {//return error if any
-            return callback({'$$':{'mtid':'error', 'errorCode':error.code, 'errorMessage': error.message}});
+    return when.promise(function(resolve, reject) {
+        //check for required params
+        if (!(url = msg.url || self.config.url)) {
+            reject(errors.configPropMustBeSet('url should be set'));
+            return;
         } else {
-            //prepare response
-            var correctResponse = {
-                $$: {mtid: 'response', opcode: msg && msg.$$ && msg.$$.opcode},
-                headers: response.headers,
-                httpStatus: response.statusCode,
-                payload: body
-            };
-            if (response.statusCode != 200) {
-                self.log && self.log.error && self.log.error('Http client request error! body: ' + body + ', statusCode: ' + response.statusCode + ', statusMessage: ' + response.statusMessage);
-                return callback({'$$':{'mtid':'error', 'errorCode': response.statusCode,
-                    'errorMessage': 'Http client: Remote server encountered an error processing the request!'
-                }});
-            }
+            url = url + (msg.uri || self.config.uri || '');
+        }
 
-            if (!body || body === '') {//if response is empty
-                correctResponse.payload = ((parseResponse) ? {} : body);
-                return callback(null, correctResponse);
+        var connProps = {
+            'followRedirect': false,
+            'method': msg.httpMethod || self.config.method,
+            'url': url,
+            'timeout': msg.requestTimeout || self.config.requestTimeout || 30000,
+            'headers': headers,
+            'body': msg.payload
+        };
+        //if there is a raw config propery it will be merged with `connProps`
+        if (self.config.raw) {
+            assign(connProps, self.config.raw);
+        }
+
+        //do the connection + request
+        request(connProps, function cbresp(error, response, body) {
+            if (error) {//return error if any
+                reject(errors.http(error));
             } else {
-                //parse the response if allowed
-                if (parseResponse) {
-                    if (!response.headers['content-type']) {
-                        return callback(errors.createUT5('MissingContentType'));
-                    } else {
-
-                        if (response.headers['content-type'].indexOf('/xml') !== -1 || response.headers['content-type'].indexOf('/soap+xml') !== -1) {
-                            return xml2js.parseString(body, {explicitArray: false}, function(err, result) {
-                                if (err) {
-                                    callback(errors.createUT5('XmlParser', err));
-                                } else {
-                                    correctResponse.payload = result
-                                    callback(null, correctResponse);
-                                }
-                            });
-                        } else if (response.headers['content-type'].indexOf('application/json') !== -1) {
-                            try {
-                                correctResponse.payload = JSON.parse(body);
-                            } catch (err) {
-                                return callback(errors.createUT5('JsonParser', err));
-                            }
-                            return callback(null, correctResponse);
+                //prepare response
+                $meta.mtid = 'response';
+                var correctResponse = {
+                    headers: response.headers,
+                    httpStatus: response.statusCode,
+                    payload: body
+                };
+                if (response.statusCode !== 200) {
+                    self.log && self.log.error && self.log.error('Http client request error! body: ' + body + ', statusCode: ' +
+                        response.statusCode + ', statusMessage: ' + response.statusMessage);
+                    var e;
+                    e = errors.http(response);
+                    e.code = response.statusCode;
+                    reject(e);
+                } else if (!body || body === '') {//if response is empty
+                    correctResponse.payload = ((parseResponse) ? {} : body);
+                    resolve(correctResponse);
+                } else {
+                    //todo is this really necessarry, probably is provided by request module already
+                    //parse the response if allowed
+                    if (parseResponse) {
+                        if (!response.headers['content-type']) {
+                            reject(errors.missingContentType());
                         } else {
-                            return callback(errors.createUT5('ParserNotFound', 'No parser found to parse response of type: ' + response.headers['content-type']));
+                            if (response.headers['content-type'].indexOf('/xml') !== -1 || response.headers['content-type'].indexOf('/soap+xml') !== -1) {
+                                xml2js.parseString(body, {explicitArray: false}, function(err, result) {
+                                    if (err) {
+                                        reject(errors.xmlParser(err));
+                                    } else {
+                                        correctResponse.payload = result;
+                                        resolve(correctResponse);
+                                    }
+                                });
+                            } else if (response.headers['content-type'].indexOf('application/json') !== -1) {
+                                try {
+                                    correctResponse.payload = JSON.parse(body);
+                                } catch (err) {
+                                    reject(errors.jsonParser(err));
+                                    return;
+                                }
+                                resolve(correctResponse);
+                            } else {
+                                reject(errors.parserNotFound('No parser found to parse response of type: ' + response.headers['content-type']));
+                            }
                         }
+                    } else {
+                        resolve(correctResponse);
                     }
                 }
-                //finally return the result
-                return callback(null, correctResponse);
             }
-        }
+        });
     });
 };
 
