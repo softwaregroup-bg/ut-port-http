5f2beee35ffdb797a80192d6e21c596f9f95cb69
Kalin Krustev <kalin.krustev@gmail.com>
2017-12-06 12:57:19 +1000

    fix: decouple from ut-bus

--- errors.js
+++ errors.js
@@ -1,49 +1,28 @@
-var create = require('ut-error').define;
+'use strict';
+module.exports = create => {
+    const PortHTTP = create('portHTTP');
+    const Generic = create('generic', PortHTTP);
+    const Parser = create('parser', PortHTTP, 'Parser Error');
 
-var PortHTTP = create('PortHTTP');
-var Generic = create('Generic',
-PortHTTP);
-var Parser = create('Parser', PortHTTP, 'Parser Error');
-var MissingContentType = create('MissingContentType', Parser, 'Server returned no content type');
-var XmlParser = create('XmlParser', Parser, 'XML Parser Error');
-var JsonParser = create('JsonParser', Parser, 'Json Parser Error');
-var ParserNotFound = create('ParserNotFound', Parser, 'Parser Not Found');
-var Config = create('Config', PortHTTP, 'Configuration error');
-var ConfigPropMustBeSet = create('ConfigPropMustBeSet', Config, 'Configuration property should be set');
-
-module.exports = {
-    http: function(response) {
-        if (response instanceof Error) {
-            return new Generic(response);
-        } else {
-            return new PortHTTP({
-                message: (response.body && response.body.message) || 'HTTP error',
-                statusCode: response.statusCode,
-                statusMessage: response.statusText,
-                validation: response.body && response.body.validation,
-                debug: response.body && response.body.debug
-            });
-        }
-    },
-    parser: function(cause) {
-        return new Parser(cause);
-    },
-    missingContentType: function(cause) {
-        return new MissingContentType(cause);
-    },
-    xmlParser: function(cause) {
-        return new XmlParser(cause);
-    },
-    jsonParser: function(cause) {
-        return new JsonParser(cause);
-    },
-    parserNotFound: function(cause) {
-        return new ParserNotFound(cause);
-    },
-    config: function(cause) {
-        return new Config(cause);
-    },
-    configPropMustBeSet: function(cause) {
-        return new ConfigPropMustBeSet(cause);
-    }
+    return {
+        http: function(response) {
+            if (response instanceof Error) {
+                return new Generic(response);
+            } else {
+                return new PortHTTP({
+                    message: (response.body && response.body.message) || 'HTTP error',
+                    statusCode: response.statusCode,
+                    statusMessage: response.statusText,
+                    validation: response.body && response.body.validation,
+                    debug: response.body && response.body.debug
+                });
+            }
+        },
+        config: create('config', PortHTTP, 'Configuration error'),
+        missingContentType: create('missingContentType', Parser, 'Server returned no content type'),
+        xmlParser: create('xmlParser', Parser, 'XML Parser Error'),
+        jsonParser: create('jsonParser', Parser, 'Json Parser Error'),
+        parserNotFound: create('parserNotFound', Parser, 'Parser Not Found'),
+        configPropMustBeSet: create('configPropMustBeSet', PortHTTP, 'Configuration property should be set')
+    };
 };
