d7f7f6c08a2e78849822257ebdc93e9f1a0da648
Elin Angelov <zetxx@belogradchik.biz>
2018-02-13 00:17:02 +0200

    fix: add logging (#17)

--- index.js
+++ index.js
@@ -87,9 +87,10 @@ module.exports = function({parent}) {
             if (this.config.raw) {
                 Object.assign(reqProps, this.config.raw);
             }
-
+            this.log && this.log.debug && this.log.debug(reqProps);
             // do the connection + request
             let req = request(reqProps, (error, response, body) => {
+                this.log && this.log.debug && this.log.debug({error, response, body});
                 if (error) { // return error if any
                     if (this.bus.config.debug) {
                         error.request = reqProps;
